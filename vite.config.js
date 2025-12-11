ef6756f4c0337c2b7c81bf657cffbee8a677c747
Henry Vélez <velezmolina@gmail.com>
2025-12-11 17:58:44 -0400

    Fixing path again

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/ServControlVanilla/',
+  base: command === 'serve' ? '/' : '/',
+  //base: command === 'serve' ? '/' : '/ServControlVanilla/',
   // config options
   plugins: [
     tailwindcss(),
